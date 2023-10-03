747f65b8b16a995e6157ce9ecd1d8e962945d956
David Christian <david.w.christian@gmail.com>
2023-10-03 09:19:10 -0700

    Saving progress

--- src/common/classes/uniswap-v2/uniswap-v2-factory/uniswap-v2-factory.class.ts
+++ src/common/classes/uniswap-v2/uniswap-v2-factory/uniswap-v2-factory.class.ts
@@ -1,7 +1,11 @@
 import { ethers } from '../../ethers';
+// import { createClient } from '@de-fi/sdk';
+// @ts-ignore
+import { GoPlus, ErrorCode } from '@goplus/sdk-node';
 import { ethers as ethersJs } from 'ethers';
 import { UniswapV2FactoryAbi } from '../../../abis/uniswap-v2';
-import { UniswapV2Address } from '../../../enums/uniswap-v2-address.enum';
+import { UniswapV2Address, UniswapV2FactoryEvent } from '../../../enums';
+import { Reporter, UniswapV2Pair, UniswapV2Erc20 } from '../../../classes';
 
 class UniswapV2Factory {
 	abi: any[];
@@ -24,6 +28,54 @@ class UniswapV2Factory {
 
 	async init() {
 		return this;
+	}
+
+	trackPairCreated() {
+		this.contract.on(
+			UniswapV2FactoryEvent.PairCreated,
+			async (token0Address, token1Address, pairAddress) => {
+				// These errored out, try dry run to test and fix..
+				// Pair Created:
+				// 0xB10D43914F80978edcFBa4c2F20Cb9dD9f4A4fFd
+				// 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+				// 0x836CD1E0af7CAc1f8592408B99e738B48A5d4Ab3
+
+				console.log('Pair Created:');
+				console.log(token0Address, token1Address, pairAddress);
+
+				// Fetch token details
+				const token0 = await new UniswapV2Erc20({
+					address: token0Address,
+				}).init();
+				console.log(token0);
+
+				const token1 = await new UniswapV2Erc20({
+					address: token1Address,
+				}).init();
+				console.log(token1);
+
+				// Fetch pair details
+				// const uniswapV2Pair = await new UniswapV2Pair({
+				// 	address: pairAddress,
+				// }).init();
+
+				// // Fetch liquidity details
+				// const reserves = await uniswapV2Pair.contract.getReserves();
+				// const { reserve0, reserve1 } = reserves;
+
+				// // TODO: Pull this out so this methods returns this data
+				// // Gloobal Store, PubSub?
+				// // Log data to console
+				// const reporter = new Reporter();
+				// reporter.logTokenPair({
+				// 	token0,
+				// 	token1,
+				// 	reserve0,
+				// 	reserve1,
+				// 	pairAddress,
+				// });
+			},
+		);
 	}
 }
 
