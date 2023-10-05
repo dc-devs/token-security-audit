4d3185d55c8b848aa7b26036410b364a45596002
David Christian <david.w.christian@gmail.com>
2023-10-05 09:23:44 -0700

    Finised typing PubSub class

--- src/common/util-classes/pub-sub/pub-sub.class.ts
+++ src/common/util-classes/pub-sub/pub-sub.class.ts
@@ -1,11 +1,29 @@
 import { PubSubEvent } from '../../enums';
+import { UniswapV2Erc20 } from '../../../token-pair-tracker/ethereum/uniswap-v2';
+
+interface PubSubCallbacks {
+	[PubSubEvent.NewTokenPairCreated]: ({
+		token0,
+		token1,
+		reserve0,
+		reserve1,
+		pairAddress,
+	}: {
+		token0: UniswapV2Erc20;
+		token1: UniswapV2Erc20;
+		reserve0: string;
+		reserve1: string;
+		pairAddress: string;
+	}) => void;
+}
 
 class PubSub {
 	eventMap = {} as Record<PubSubEvent, Set<(...args: any[]) => void>>;
 
-	constructor() {}
-
-	on(event: PubSubEvent, callback: (...args: any[]) => void) {
+	on<K extends keyof PubSubCallbacks>(
+		event: K,
+		callback: PubSubCallbacks[K],
+	) {
 		if (!this.eventMap[event]) {
 			// create a new set
 			this.eventMap[event] = new Set();
@@ -14,7 +32,10 @@ class PubSub {
 		this.eventMap[event].add(callback);
 	}
 
-	off(event: PubSubEvent, callback: (...args: any[]) => void) {
+	off<K extends keyof PubSubCallbacks>(
+		event: K,
+		callback: PubSubCallbacks[K],
+	) {
 		if (!this.eventMap[event]) {
 			return;
 		}
@@ -22,7 +43,10 @@ class PubSub {
 		this.eventMap[event].delete(callback);
 	}
 
-	emit(event: PubSubEvent, ...args: any[]) {
+	emit<K extends keyof PubSubCallbacks>(
+		event: K,
+		...args: Parameters<PubSubCallbacks[K]>
+	) {
 		if (!this.eventMap[event]) {
 			return;
 		}
